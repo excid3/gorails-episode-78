4abd2aa2ea99828442bf404abf567b7c6dfb44f9
Rodrigo Argumedo <rodrigo.argumedo@icloud.com>
2015-07-27 14:57:05 -0600

    Return the username id instead a numbered ID

--- db/migrate/20150717174216_create_users.rb
+++ db/migrate/20150717174216_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :users do |t|
+    create_table :users, id: false do |t|
       t.string :username
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20150717174221) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: false, force: :cascade do |t|
     t.string   "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
